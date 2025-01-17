6026ccd349c0cda568b37359bcef29afc1889a31
Class9123 <jayjaygovind4@gmail.com>
2025-01-17 10:47:50 +0545

    Changed

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axio = axios.create({
-  baseURL:"http://localhost:5000" ,//"https://backend-22cx.onrender.com/" ,//'http://localhost:5000
+  baseURL:"https://backend-22cx.onrender.com/" ,
   headers: {
     'Content-Type': 'application/json',
   },
--- src/lib/socket.js
+++ src/lib/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const SOCKET_SERVER_URL ="http://localhost:5000" //"https://backend-22cx.onrender.com/" //"http://localhost:5000";
+const SOCKET_SERVER_URL ="https://backend-22cx.onrender.com/" //"http://localhost:5000";
 const socket = io(SOCKET_SERVER_URL);
 
 export default socket;
