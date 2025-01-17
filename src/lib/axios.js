5ea61ff13c5f8c82fd1b4a2897010ca468a48394
jay Govind <163959445+Class9123@users.noreply.github.com>
2025-01-17 17:49:10 +0545

    Update axios.js

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -4,7 +4,7 @@ const axio = axios.create({
   headers: {
     'Content-Type': 'application/json',
   },
-  baseURL:"http://localhost:5000"// "https://backend-22cx.onrender.com/" ,
+  baseURL:"https://backend-22cx.onrender.com/" ,
 });
 
 export default axio;
